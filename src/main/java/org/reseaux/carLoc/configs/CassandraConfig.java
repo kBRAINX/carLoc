879e91d6b7ffd7c948c7571109f742f1bca9e005
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2024-06-26 05:08:37 +0100

    update scylla configuration

--- src/main/java/org/reseaux/carLoc/configs/CassandraConfig.java
+++ src/main/java/org/reseaux/carLoc/configs/CassandraConfig.java
@@ -1,17 +1,28 @@
 package org.reseaux.carLoc.configs;
 
-import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
+import org.springframework.data.cassandra.config.SessionFactoryFactoryBean;
 import org.springframework.data.cassandra.core.CassandraAdminTemplate;
 import org.springframework.data.cassandra.core.CassandraTemplate;
+import org.springframework.data.cassandra.core.convert.MappingCassandraConverter;
+import org.springframework.data.cassandra.core.mapping.CassandraMappingContext;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
+
+import java.util.Objects;
 
 @Configuration
 @EnableCassandraRepositories(basePackages = "org.reseaux.carLoc.repositories")
 public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.cassandra.contact-points}")
+    private String contactPoints;
+
+    @Value("${spring.cassandra.port}")
+    private int port;
 
     @Value("${spring.cassandra.keyspace-name}")
     private String keyspaceName;
@@ -29,16 +40,36 @@ public class CassandraConfig extends AbstractCassandraConfiguration {
         return localDatacenter;
     }
 
+    @Override
+    protected String getContactPoints() {
+        return contactPoints;
+    }
+
+    @Override
+    protected int getPort() {
+        return port;
+    }
+
     @Bean
-    public CqlSession session() {
-        return CqlSession.builder()
-            .withKeyspace(keyspaceName)
-            .withLocalDatacenter(localDatacenter)
-            .build();
+    public CqlSessionFactoryBean session() {
+        CqlSessionFactoryBean session = new CqlSessionFactoryBean();
+        session.setContactPoints(contactPoints);
+        session.setPort(port);
+        session.setKeyspaceName(keyspaceName);
+        return session;
+    }
+
+    @Bean
+    public SessionFactoryFactoryBean sessionFactory(CqlSessionFactoryBean session, CassandraMappingContext context) {
+        SessionFactoryFactoryBean sessionFactory = new SessionFactoryFactoryBean();
+        sessionFactory.setSession(Objects.requireNonNull(session.getObject()));
+        sessionFactory.setConverter(new MappingCassandraConverter(context));
+        sessionFactory.setSchemaAction(getSchemaAction());
+        return sessionFactory;
     }
 
     @Bean
     public CassandraAdminTemplate cassandraTemplate() {
-        return (CassandraAdminTemplate) new CassandraTemplate(session());
+        return (CassandraAdminTemplate) new CassandraTemplate(Objects.requireNonNull(session().getObject()));
     }
 }
