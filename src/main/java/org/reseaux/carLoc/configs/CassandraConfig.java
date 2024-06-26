c78f3060f43b654c867e65d6e468b83502fbc287
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2024-06-26 04:48:47 +0100

    update scylla configuration

--- src/main/java/org/reseaux/carLoc/configs/CassandraConfig.java
+++ src/main/java/org/reseaux/carLoc/configs/CassandraConfig.java
@@ -1,31 +1,44 @@
 package org.reseaux.carLoc.configs;
 
 import com.datastax.oss.driver.api.core.CqlSession;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.cassandra.config.AbstractCassandraConfiguration;
+import org.springframework.data.cassandra.core.CassandraAdminTemplate;
 import org.springframework.data.cassandra.core.CassandraTemplate;
 import org.springframework.data.cassandra.repository.config.EnableCassandraRepositories;
 
-import java.util.Objects;
-import java.util.ResourceBundle;
-
 @Configuration
 @EnableCassandraRepositories(basePackages = "org.reseaux.carLoc.repositories")
-public class CassandraConfig {
+public class CassandraConfig extends AbstractCassandraConfiguration {
+
+    @Value("${spring.cassandra.keyspace-name}")
+    private String keyspaceName;
+
+    @Value("${spring.cassandra.local-datacenter}")
+    private String localDatacenter;
+
+    @Override
+    protected String getKeyspaceName() {
+        return keyspaceName;
+    }
+
+    @Override
+    protected String getLocalDataCenter() {
+        return localDatacenter;
+    }
 
     @Bean
     public CqlSession session() {
-        return CqlSession
-                .builder()
-                .withKeyspace("reseau")
-                .withLocalDatacenter("datacenter1")
-                .build();
+        return CqlSession.builder()
+            .withKeyspace(keyspaceName)
+            .withLocalDatacenter(localDatacenter)
+            .build();
     }
 
     @Bean
-    public CassandraTemplate cassandraTemplate() throws Exception {
-        return new CassandraTemplate(session());
+    public CassandraAdminTemplate cassandraTemplate() {
+        return (CassandraAdminTemplate) new CassandraTemplate(session());
     }
 }
-
