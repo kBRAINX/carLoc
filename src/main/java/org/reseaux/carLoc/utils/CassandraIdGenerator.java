59213cfa0f85de9aed670c759e071378ffd2512d
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2024-07-01 23:59:45 +0100

    updated

--- src/main/java/org/reseaux/carLoc/utils/CassandraIdGenerator.java
+++ src/main/java/org/reseaux/carLoc/utils/CassandraIdGenerator.java
@@ -2,6 +2,7 @@ package org.reseaux.carLoc.utils;
 
 import com.datastax.oss.driver.api.core.CqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.cassandra.config.CqlSessionFactoryBean;
 import org.springframework.stereotype.Component;
 
@@ -11,7 +12,7 @@ public class CassandraIdGenerator {
     private final CqlSession cqlSession;
 
     @Autowired
-    public CassandraIdGenerator(CqlSession cqlSession) {
+    public CassandraIdGenerator(@Qualifier("session") CqlSession cqlSession) {
         this.cqlSession = cqlSession;
     }
 
