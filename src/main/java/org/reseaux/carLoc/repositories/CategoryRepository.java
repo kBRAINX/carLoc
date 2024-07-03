2296cef79b6bd079b76936dbeedc98039464b9de
KBRAINX <aurel.nguepssi@facsciences-uy1.cm>
2024-07-03 06:26:31 +0100

    updated

--- src/main/java/org/reseaux/carLoc/repositories/CategoryRepository.java
+++ src/main/java/org/reseaux/carLoc/repositories/CategoryRepository.java
@@ -1,7 +1,6 @@
 package org.reseaux.carLoc.repositories;
 
 import org.reseaux.carLoc.models.Category;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-@Primary
 public interface CategoryRepository extends CassandraRepository<Category, Long> {
     @AllowFiltering
     List<Category> findByAgenceId(long agenceId);
--- src/main/java/org/reseaux/carLoc/repositories/PosteRepository.java
+++ src/main/java/org/reseaux/carLoc/repositories/PosteRepository.java
@@ -1,7 +1,6 @@
 package org.reseaux.carLoc.repositories;
 
 import org.reseaux.carLoc.models.Poste;
-import org.springframework.context.annotation.Primary;
 import org.springframework.data.cassandra.repository.AllowFiltering;
 import org.springframework.data.cassandra.repository.CassandraRepository;
 import org.springframework.stereotype.Repository;
@@ -9,7 +8,6 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-@Primary
 public interface PosteRepository extends CassandraRepository<Poste, Long> {
     @AllowFiltering
     List<Poste> findByAgenceId(long agenceId);
